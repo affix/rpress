8b8b03b8c659b7643aaee3baf57b930fb96aed5d
Keiran Smith <affix@affix.me>
2014-03-16 20:11:57 +0000

    Using bootstrap dist

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,14 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
+//= require bootstrap/affix.js
+//= require bootstrap/dropdown.js
+//= require bootstrap/collapse.js
+//= require bootstrap/button.js
+//= require bootstrap/scrollspy.js
+//= require bootstrap/tab.js
+//= require bootstrap/transition.js
+//= require bootstrap/tooltip.js
+//= require bootstrap/popover.js
 //= require turbolinks
 //= require_tree .
